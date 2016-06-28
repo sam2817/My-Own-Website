b4cbad2df43ed98513fd8f29216bcd2bd52d6c63
sam2817 <sam2817@users.noreply.github.com>
2016-06-28 11:58:50 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var navHome = document.getElementById("zero");
+var navAboutMe = document.getElementById("degZero");
+var navInterests = document.getElementById("degThirty");
+var navLinks = document.getElementById("degSixty");
+var navComments = document.getElementById("degNinty");
+
+navHome.addEventListener("click", myFunctionHome);
+navAboutMe.addEventListener("click", myFunctionAboutMe);
+navInterests.addEventListener("click", myFunctionInterests);
+navLinks.addEventListener("click", myFunctionLinks);
+navComments.addEventListener("click", myFunctionComments);
+
+myFunctionHome {
+	
+}
+
+myFunctionAboutMe {
+	
+}
+
+myFunctionInterests {
+	
+}
+
+myFunctionLinks {
+	
+}
+
+myFunctionComments {
+	
+}
